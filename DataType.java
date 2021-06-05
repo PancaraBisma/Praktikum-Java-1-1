3b9f3357d9df113888a8fca27e252d1e456a3655
PancaraBisma <pancarabisma@gmail.com>
2021-06-05 18:57:34 +0800

    deklarasi variabel dan tipe data

---
+++ DataType.java
@@ -0,0 +1,13 @@
+public class DataType{
+    public static void main(String[] args) {
+
+        String namaDepan = "Muhammad";
+        String namaTengah = "Pancara";
+        String namaBelakang = "Bisma";
+        int usia = 19;
+        int targetTahunKuliah = 4;
+        double ipk = 3.821452;
+        char nilaAbjad = 'A';
+        boolean tampan = true;
+    }
+}
